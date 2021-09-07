9c9f6b4a9df7b77d128668333a27d07e842c8938
Nick Brooks <nbrooks1@progressive.com>
2021-09-07 16:18:41 -0400

    Example 1

---
+++ EmployeesApi/Controllers/StatusController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesApi.Controllers
+{
+    public class StatusController : ControllerBase
+    {
+        [HttpGet("status")]
+        public ActionResult GetTheStatus()
+        {
+            var response = new GetStatusResponse
+            {
+                Message = "Running Great!",
+                WhenChecked = DateTime.Now
+            };
+            return Ok(response);
+        }
+
+    }
+
+
+    public class GetStatusResponse
+    {
+        public string Message { get; set; }
+        public DateTime WhenChecked { get; set; }
+    }
+
+
+}
